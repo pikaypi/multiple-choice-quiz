4785162b3cff93279358e807166a096ead3c1fae
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-04 00:16:30 -0400

    seed the game with content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,37 @@
 const displayEl = document.getElementById('display');
 const timerEl = document.getElementById('timer');
 
+// A variable to hold the content of the quiz
+const questions = [
+    {
+        message: 'A(n) _________ tag is used to define an interactive field where users can enter data.',
+        options: [
+            ['<datalist>', 'incorrect'],
+            ['<input>', 'correct'],
+            ['<enterpoint>', 'incorrect'],
+            ['<dialog>', 'incorrect']
+        ]
+    },
+    {
+        message: 'The ________ CSS property is used to add space around sections of content.',
+        options: [
+            ['Spacing', 'incorrect'],
+            ['Cleaner', 'incorrect'],
+            ['Break', 'incorrect'],
+            ['Padding', 'correct']
+        ]
+    },
+    {
+        message: 'Javascript files are linked to html with _________ tags',
+        options: [
+            ['<script>', 'correct'],
+            ['<javascript>', 'incorrect'],
+            ['<js>', 'incorrect'],
+            ['<scripting>', 'incorrect']
+        ]
+    }
+];
+
 // A function that clears the display element
 const clearDisplay = () => {
     while (displayEl.firstChild) {
